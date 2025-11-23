e00eb3ee88a79e802ad1a9b9cc282e142e9af750
Romina <romina.cornejo@usm.cl>
2025-11-22 23:24:45 -0300

    feat: Initialize First-Fit algorithm files structure.

---
+++ include/first-fit.h
@@ -0,0 +1,13 @@
+#ifndef FIRST_FIT_H
+#define FIRST_FIT_H
+
+#include "best-fit-tdas.h"
+
+int holes_list_request_first_fit(
+    hole_t** head_holes_list, hole_t** tail_holes_list,
+    used_position_t** head_used_list, used_position_t** tail_used_list,
+    unsigned int requested_size,
+    unsigned int pid
+);
+
+#endif
