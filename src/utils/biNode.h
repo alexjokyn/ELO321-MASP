0d836ebf27096bc9d7413761bceef103e3e14191
Joaquin Jara <alexjokyn@gmail.com>
2025-11-23 21:05:26 -0300

    chore: remove unused biNode module
    biNode module was never used. Also, src/utils is being deleted and now short modules will be stored inside the ./utils directory

--- src/utils/biNode.h
+++
@@ -1,9 +0,0 @@
-typedef struct biNode biNode;
-
-void push(int, biNode**);
-int valueAt(int, biNode*);
-int pop(biNode**);
-int popValue(int, biNode**);
-int popAt(int, biNode**);
-void printList(biNode*);
-void clear(biNode**);
